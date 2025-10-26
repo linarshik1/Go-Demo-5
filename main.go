b6a1bbb877f1491229dced8eba41f3fa2b115c54
linarshik1 <linarmakh20.06@gmail.com>
2025-10-26 21:34:23 +0300

    15.x - http request

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"demo/weather/geo"
+	"demo/weather/weather"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Новый проект")
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(geoData)
+	weatherData := weather.GetWeather(*geoData, *format)
+	fmt.Println(weatherData)
+}
---
+++ weather/weather.go
@@ -0,0 +1,32 @@
+package weather
+
+import (
+	"demo/weather/geo"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func GetWeather(geo geo.GeoData, format int) string {
+	baseUrl, err := url.Parse("https://wttr.in/" + geo.City)
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	params := url.Values{}
+	params.Add("format", fmt.Sprint(format))
+	baseUrl.RawQuery = params.Encode()
+	resp, err := http.Get(baseUrl.String())
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	return string(body)
+}
